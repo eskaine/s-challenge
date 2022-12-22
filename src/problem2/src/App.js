15a5eaa7e826fe2c6ba02aab088e451059dfbe4b
Eskaine <eskaineliu@gmail.com>
2022-12-22 21:36:48 +0800

    added header

--- src/problem2/src/App.js
+++ src/problem2/src/App.js
@@ -1,6 +1,14 @@
 import { useState } from 'react';
-import { Box, Button, Grid } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import InputComponent from './components/inputComponent';
+
+const headerStyle = {
+  marginBottom: 3,
+  textAlign: 'center',
+  fontWeight: 'bold',
+  fontSize: 20,
+  color: '#66BB6A'
+}
 
 const boxStyle = {
   width: 300,
@@ -11,6 +19,7 @@ const boxStyle = {
 };
 
 const submitBtnStyle = {
+  marginTop: 2,
   padding: 1,
   borderRadius: 5,
   backgroundImage: 'linear-gradient(45deg, #2ECC71, #1ABC9C)',
@@ -39,7 +48,9 @@ function App() {
 
   return (
     <div className="App">
+
       <Box sx={boxStyle}>
+        <Typography sx={headerStyle}>SEND TRANSACTION</Typography>
         <Grid container spacing={2}>
           <InputComponent 
             labelName="ETH Address" 
@@ -55,7 +66,7 @@ function App() {
             onChange={otpOnChangeHandler}
           />
           <Grid item xs={12}>
-            <Button fullWidth sx={submitBtnStyle} variant="contained">SEND TOKEN</Button>
+            <Button fullWidth sx={submitBtnStyle} variant="contained">SEND</Button>
           </Grid>
         </Grid>
       </Box>
